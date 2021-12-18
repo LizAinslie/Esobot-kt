5e737028fb05dcd337837ce3b0d58fc7542b9955
Liz Ainslie <lizainslie16@gmail.com>
2021-12-18 10:14:52 -0600

    Fix config serialization

--- src/main/kotlin/dev/lizainslie/esobot/EsoBotConfig.kt
+++ src/main/kotlin/dev/lizainslie/esobot/EsoBotConfig.kt
@@ -1,8 +1,10 @@
 package dev.lizainslie.esobot
 
 import dev.lizainslie.botlin.BaseConfig
+import kotlinx.serialization.Serializable
 
+@Serializable
 data class EsoBotConfig(
-    override val revoltToken: String?,
-    override val discordToken: String?
+    override val revoltToken: String? = null,
+    override val discordToken: String? = null,
 ) : BaseConfig()
